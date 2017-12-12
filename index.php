10b6bf617bcbc2d70987f7eea3e3c2f7ee839096
Archie <archie.isdiningrat@bizzy.co.id>
2017-12-12 15:59:46 +0700

    add files

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   <head>
     <title>Spatial Database</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBPxbbcPu_GZsznOXsbaGhnk5ig2631xMM"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBPxbbcPu_GZsznOXsbaGhnk5ig2631xMM&libraries=places"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
@@ -25,50 +25,45 @@
     </div>
 
     <script>
+      var telkom = {lat: -6.973996, lng: 107.630207};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom:12,
+        center: telkom
+      });
+
       function initMap() {
-        var uluru = {lat: -6.973996, lng: 107.630207};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom:14,
-          center: uluru
-        });
+        var request = {
+          location: telkom,
+          radius: '3000',
+          types: ['gas_station']
+        };
 
-        var start;
-        $.ajax({
-          dataType: "json",
-          url: 'http://localhost:8000/kfn.php',
-          success: function(data){
-            start = data.start;
-            finish = data.finish;
-            max = data.max + 1;
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
 
-            finish.forEach(function(item) {
-
-              if(max === item.index){
-                icn = 'http://localhost:8000/icon/mark.png';
-              }else{
-                icn = '';
-              }
-
-                var marker = new google.maps.Marker({
-                  position: item.coordinate,
-                  label: item.index,
-                  map: map,
-                  icon:icn,
-                  title: item.distance
-                });
-
-            });
-
-            var image = 'http://localhost:8000/icon/home.png';
-            var marker = new google.maps.Marker({
-              position: start,
-              map: map,
-              icon:image
-            });
-          }
+        var start = telkom;
+        new google.maps.Marker({
+          position: start,
+          map: map
         });
 
       }
+
+      function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            new google.maps.Marker({
+              map: map,
+              place: {
+                placeId: results[i].place_id,
+                location: results[i].geometry.location
+              }
+            });
+          }
+        }
+      }
+
       initMap();
 
     </script>
