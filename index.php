30f7c9fcb159916baf7a06aa0f720b1210b06606
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2017-12-12 14:59:29 +0700

    update maps key

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   <head>
     <title>Spatial Database</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJof97nZEkcDATIPrVKYXZo2isp-BsdW0"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBPxbbcPu_GZsznOXsbaGhnk5ig2631xMM"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
